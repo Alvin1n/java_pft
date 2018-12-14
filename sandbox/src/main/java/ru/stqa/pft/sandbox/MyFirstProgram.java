d87453573580567d560a07dfbe54d70f1077f55a
Alvin1n <test-ip@mail.ru>
2018-12-14 22:26:07 +0500

    добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,6 +6,19 @@ class MyFirstProgram {
 
     l++;
     System.out.println("Hello, java" + l);
-
+    hello("world");
+    hello("no   understand");
+    double len=55, q=66, d=44;
+    System.out.println("Площадь квадрата = " + areas(len));
+    System.out.println("Обьем паралелепипеда = " + areas(len, q, d));
+  }
+  public static void hello(String some) {
+    System.out.println("Hello, "+some);
+  }
+  public static double areas(double a) {
+    return a*a;
+  }
+  public static double areas(double a, double b, double c){
+    return a*b*c;
   }
 }
