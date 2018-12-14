b74d6715b0dfa306872ebadeb114515deb391e20
Alvin1n <test-ip@mail.ru>
2018-12-14 19:51:49 +0500

    изменен отступ на 2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+class MyFirstProgram {
+  public static void main(String args[]) {
+    int l = 11;
+
+    l++;
+    System.out.println("Hello, java" + l);
+
+  }
+}
